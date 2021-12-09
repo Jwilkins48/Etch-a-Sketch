f1fe38e1aa65f44d3447293220040571d4ac7f33
Jenna <jenna48071@gmail.com>
2021-12-09 16:34:53 -0600

    Added functioning to slider and button

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let amountDefault = 16;
+// Variables
+const btn = document.getElementById('restartBtn');
+let slider = document.getElementById('sliderRange');
+let sliderValue = document.getElementById('sliderRange').value;
+let sliderContainer = document.getElementById('sliderContainer');
+let amountDefault = 25;
+
 
 function createGrid(amount) {
     const container = document.querySelector('#container');
-
+    //Creating rows and columns
     container.style.gridTemplateRows = `repeat(${amount}, 1fr)`;
     container.style.gridTemplateColumns = `repeat(${amount}, 1fr)`;
 
@@ -12,13 +18,20 @@ function createGrid(amount) {
         container.appendChild(grid);
     };
 };
-
+// Change into random colors
 function changeColor(e) {
     let random1 = Math.floor(Math.random() * 256)
     let random2 = Math.floor(Math.random() * 256)
     let random3 = Math.floor(Math.random() * 256)
     e.target.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
 };
+//Changing the sliders value
+function sliderChange(val) {
+    sliderValue = val;
+    createGrid(sliderValue);
+}
 
+slider.addEventListener('mouseup', sliderChange);
+btn.addEventListener('click', () => location.reload());
 
 createGrid(amountDefault);
